8b6e7e711074fdb77b8ad923aa195a68a4fc8913
EC2 Default User <ec2-user@ip-172-31-17-41.ec2.internal>
2026-08-17 02:57:21 +0000

    welcome lokesh

---
+++ terraform1/main.tf
@@ -0,0 +1,49 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "webserver" {
+  ami           = "ami-0bdc7d025135d7b49"
+  instance_type = "c7a.large"
+  key_name      = "azlin"
+
+  tags = {
+    Name = "Terraform-WebServer"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install httpd -y
+              systemctl start httpd
+              systemctl enable httpd
+              EOF
+}
+resource "aws_security_group" "web_sg" {
+  name = "web-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.webserver.public_ip
+}
